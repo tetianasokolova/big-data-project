2bb73cca984b46023f8c22902c19a4e51beeafdc
Yuliia Stoliariuk <uliastolaruk@gmail.com>
2024-10-04 13:59:09 +0300

    Add python package 'basic_dfs' and update main.py by adding spark_session

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("This is the project of Mishchenia, Sokolova, Stoliaruk")
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+spark_session = (SparkSession.builder
+                 .master("local")
+                 .appName("task app")
+                 .config(conf=SparkConf())
+                 .getOrCreate())
+
+spark_session.stop()
