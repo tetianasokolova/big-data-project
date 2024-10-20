cfbffc4f81fb54ac0fa8fe94ae66cb0a560e3c94
VovaMishchenia23 <vova2004mis@gmail.com>
2024-10-20 12:48:02 +0300

    comment old code in main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from pyspark import SparkConf
-from pyspark.sql import SparkSession
-from basic_dfs import basic_df_stoliaruk
-from basic_dfs import basic_df_sokolova
-from basic_dfs import basic_df_mishcenia
-
-spark_session = (SparkSession.builder
-                 .master("local")
-                 .appName("task app")
-                 .config(conf=SparkConf())
-                 .getOrCreate())
-
-basic_df_stoliaruk.basic_test_df().show()
-basic_df_sokolova.basic_test_df().show()
-basic_df_mishcenia.basic_test_df().show()
-spark_session.stop()
+# from pyspark import SparkConf
+# from pyspark.sql import SparkSession
+# from basic_dfs import basic_df_stoliaruk
+# from basic_dfs import basic_df_sokolova
+# from basic_dfs import basic_df_mishcenia
+#
+# spark_session = (SparkSession.builder
+#                  .master("local")
+#                  .appName("task app")
+#                  .config(conf=SparkConf())
+#                  .getOrCreate())
+#
+# basic_df_stoliaruk.basic_test_df().show()
+# basic_df_sokolova.basic_test_df().show()
+# basic_df_mishcenia.basic_test_df().show()
+# spark_session.stop()
