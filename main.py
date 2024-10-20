28270f87612a262dbd9543b76afd440150e0df31
Tetiana Sokolova <tanya.sokolova1406@gmail.com>
2024-10-20 22:29:13 +0200

    Delete show actions

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ from dfs_postprocess.title_principals_postprocess import title_principals_postpr
 title_principals_df = read_title_principals_df(TITLE_PRINCIPALS_DF_PATH)
 title_principals_df = title_principals_postprocess(title_principals_df)
 write_title_principals_df_to_csv(title_principals_df, TITLE_PRINCIPALS_RESULTS_PATH)
-title_principals_df.show()
 
 name_basics_df = read_name_basics_df(NAME_BASICS_DF_PATH)
 name_basics_df = name_basics_postprocess(name_basics_df)
 write_name_basics_df_to_csv(name_basics_df, NAME_BASICS_DF_RESULTS_PATH)
-name_basics_df.show()
