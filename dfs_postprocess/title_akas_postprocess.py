287db30607095afede71eae9c7b7fd8c2ca50c9f
VovaMishchenia23 <vova2004mis@gmail.com>
2024-10-20 12:30:02 +0300

    Add dfs_postprocess packages

---
+++ dfs_postprocess/title_akas_postprocess.py
@@ -0,0 +1,10 @@
+def change_column_names(title_akas_df):
+    pass
+
+def change_type_names(title_akas_df):
+    pass
+
+def title_akas_postprocess(title_akas_df):
+    title_akas_df=change_column_names(title_akas_df)
+    title_akas_df=change_type_names(title_akas_df)
+    return title_akas_df
