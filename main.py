ad4213ecdfc44f019108c86160928fc05dda1045
VovaMishchenia23 <vova2004mis@gmail.com>
2024-10-20 17:20:49 +0300

    create functions for read/write title.crew.tsv file

--- main.py
+++ main.py
@@ -15,12 +15,15 @@
 # basic_df_mishcenia.basic_test_df().show()
 # spark_session.stop()
 
-from setting import AKAS_DF_PATH,AKAS_DF_RESULTS_PATH
+from setting import AKAS_DF_PATH, AKAS_DF_RESULTS_PATH, TITLE_CREW_DF_PATH, TITLE_CREW_DF_RESULTS_PATH
 
-from my_io import read_akas_df,write_akas_df_to_csv
+from my_io import read_akas_df, write_akas_df_to_csv, read_title_crew_df, write_title_crew_df_to_csv
 from dfs_postprocess.title_akas_postprocess import title_akas_postprocess
 
-akas_df=read_akas_df(AKAS_DF_PATH)
-akas_df=title_akas_postprocess(akas_df)
-akas_df.show()
+akas_df = read_akas_df(AKAS_DF_PATH)
+akas_df = title_akas_postprocess(akas_df)
 write_akas_df_to_csv(akas_df,AKAS_DF_RESULTS_PATH)
+
+title_crew_df = read_title_crew_df(TITLE_CREW_DF_PATH)
+
+
--- my_io.py
+++ my_io.py
@@ -10,19 +10,41 @@ def read_akas_df(path):
                      .appName("task app")
                      .config(conf=SparkConf())
                      .getOrCreate())
-    akas_df_schema=t.StructType([
-        t.StructField('titleId',t.StringType(),False),
-        t.StructField('ordering',t.IntegerType(),False),
-        t.StructField('title',t.StringType(),False),
-        t.StructField('region',t.StringType(),True),
-        t.StructField('language',t.StringType(),True),
-        t.StructField('types',t.StringType(),True),
-        t.StructField('attributes',t.StringType(),True),
-        t.StructField('isOriginalTitle',t.IntegerType(),False) #при переведенні в boolean всюди простаавляються null тому залишаю Integer
-                                 ])
-    akas_df = spark_session.read.option('sep', '\t').csv(path,header=True,nullValue='null',schema=akas_df_schema)
+    akas_df_schema = t.StructType([
+        t.StructField('titleId', t.StringType(), False),
+        t.StructField('ordering', t.IntegerType(), False),
+        t.StructField('title', t.StringType(), False),
+        t.StructField('region', t.StringType(), True),
+        t.StructField('language', t.StringType(), True),
+        t.StructField('types', t.StringType(), True),
+        t.StructField('attributes', t.StringType(), True),
+        t.StructField('isOriginalTitle', t.IntegerType(), False)
+        # при переведенні в boolean всюди простаавляються null тому залишаю Integer
+    ])
+    akas_df = spark_session.read.option('sep', '\t').csv(path, header=True, nullValue='null', schema=akas_df_schema)
 
     return akas_df
 
-def write_akas_df_to_csv(df,path):
-    df.write.csv(path,header=True,mode='overwrite',encoding='utf-8',sep=',',emptyValue='nan')
+
+def read_title_crew_df(path):
+    spark_session = (SparkSession.builder
+                     .master("local")
+                     .appName("task app")
+                     .config(conf=SparkConf())
+                     .getOrCreate())
+    title_crew_df_schema = t.StructType([
+        t.StructField('tconst', t.StringType(), nullable=False),
+        t.StructField('directors', t.StringType(), False),
+        t.StructField('writers', t.StringType(), True)])
+    title_crew_df = spark_session.read.option('sep', '\t').csv(path, header=True, nullValue='null',
+                                                               schema=title_crew_df_schema)
+
+    return title_crew_df
+
+
+def write_title_crew_df_to_csv(df, path):
+    df.write.csv(path, header=True, mode='overwrite', encoding='utf-8', sep=',')
+
+
+def write_akas_df_to_csv(df, path):
+    df.write.csv(path, header=True, mode='overwrite', encoding='utf-8', sep=',', emptyValue='nan')
