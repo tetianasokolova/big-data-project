444409914dfe1fcf99535781df2b07d1035d76d1
Tetiana Sokolova <tanya.sokolova1406@gmail.com>
2024-10-20 21:54:00 +0200

    Add change_characters_column function

--- dfs_postprocess/title_principals_postprocess.py
+++ dfs_postprocess/title_principals_postprocess.py
@@ -11,7 +11,14 @@ def change_n_to_none(title_principals_df):
                                                  f.when(f.col(column).isin("\\N"), None).otherwise(f.col(column)))
     return title_principals_df
 
+def change_characters_column(title_principals_df):
+    title_principals_df = title_principals_df.withColumn('characters',
+                                                         f.regexp_replace(
+                                                             f.col('characters'), r'[\[\]"]', ""))
+    return title_principals_df
+
 def title_principals_postprocess(title_principals_df):
     title_principals_df = change_column_names(title_principals_df)
     title_principals_df = change_n_to_none(title_principals_df)
+    title_principals_df = change_characters_column(title_principals_df)
     return title_principals_df
