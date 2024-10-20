0719427576f6bf97178f8c2535905f201ff49001
Tetiana Sokolova <tanya.sokolova1406@gmail.com>
2024-10-20 20:39:18 +0200

    Add split_multivalued_columns function

--- dfs_postprocess/name_basics_postprocess.py
+++ dfs_postprocess/name_basics_postprocess.py
@@ -13,6 +13,14 @@ def change_n_to_none(name_basics_df):
                                                f.when(f.col('death_year').isin("\\N"), None).otherwise(f.col('death_year')))
     return name_basics_df
 
+# Split columns primary_profession and known_for_titles with multiple values into rows
+def split_multivalued_columns(name_basics_df):
+    name_basics_df = name_basics_df.withColumn('primary_profession',
+                                               f.explode(f.split(f.col('primary_profession'), ",")))
+    name_basics_df = name_basics_df.withColumn('known_for_titles',
+                                               f.explode(f.split(f.col('known_for_titles'), ",")))
+    return name_basics_df
+
 def name_basics_postprocess(name_basics_df):
     name_basics_df = change_column_names(name_basics_df)
     name_basics_df = change_n_to_none(name_basics_df)
