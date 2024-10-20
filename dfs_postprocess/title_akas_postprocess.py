4e8c5a4a8853e3a6b36710d6f2f9e9c1c45cd4d1
VovaMishchenia23 <vova2004mis@gmail.com>
2024-10-20 16:59:20 +0300

    add cahange_is_original_title_column_type function

--- dfs_postprocess/title_akas_postprocess.py
+++ dfs_postprocess/title_akas_postprocess.py
@@ -6,8 +6,13 @@ def change_column_names(title_akas_df):
                                                                                              'is_original_title')
     return title_akas_df
 
+def change_is_original_title_column_type(title_akas_df):
+    return title_akas_df.withColumn('is_original_title', title_akas_df['is_original_title'].cast('boolean'))
+
+
 
 def title_akas_postprocess(title_akas_df):
     title_akas_df = change_column_names(title_akas_df)
+    title_akas_df = change_is_original_title_column_type(title_akas_df)
 
     return title_akas_df
