ef25948fb5c1c551a5ef16c9e731b0d887962e6c
Tetiana Sokolova <tanya.sokolova1406@gmail.com>
2024-10-20 22:26:39 +0200

    Modify constant path names

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from setting import (TITLE_PRINCIPALS_DF_PATH, TITLE_PRINCIPALS_RESULT_PATH,
-                     NAME_BASICS_DF_PATH, NAME_BASICS_DF_RESULT_PATH)
+from setting import (TITLE_PRINCIPALS_DF_PATH, TITLE_PRINCIPALS_RESULTS_PATH,
+                     NAME_BASICS_DF_PATH, NAME_BASICS_DF_RESULTS_PATH)
 from my_io import (read_title_principals_df, write_title_principals_df_to_csv,
                    read_name_basics_df, write_name_basics_df_to_csv)
 from dfs_postprocess.name_basics_postprocess import name_basics_postprocess
@@ -7,10 +7,10 @@ from dfs_postprocess.title_principals_postprocess import title_principals_postpr
 
 title_principals_df = read_title_principals_df(TITLE_PRINCIPALS_DF_PATH)
 title_principals_df = title_principals_postprocess(title_principals_df)
-write_title_principals_df_to_csv(title_principals_df, TITLE_PRINCIPALS_RESULT_PATH)
+write_title_principals_df_to_csv(title_principals_df, TITLE_PRINCIPALS_RESULTS_PATH)
 title_principals_df.show()
 
 name_basics_df = read_name_basics_df(NAME_BASICS_DF_PATH)
 name_basics_df = name_basics_postprocess(name_basics_df)
-write_name_basics_df_to_csv(name_basics_df, NAME_BASICS_DF_RESULT_PATH)
+write_name_basics_df_to_csv(name_basics_df, NAME_BASICS_DF_RESULTS_PATH)
 name_basics_df.show()
