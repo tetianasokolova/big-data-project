3f7fc20b112f15e43b70eef4178e8865917bcc9c
Tetiana Sokolova <tanya.sokolova1406@gmail.com>
2024-10-27 21:39:46 +0100

    Add title_rating_with_votes_above_10000 function

--- questions/questions_sokolova.py
+++ questions/questions_sokolova.py
@@ -1,5 +1,10 @@
 import pyspark.sql.functions as f
 
+# question 24: які унікальні ідентифікатори та рейтинги у фільмів, що мають більше як 10,000 голосів?
+def title_rating_with_votes_above_10000(title_rating_df):
+    return (title_rating_df.filter(f.col('num_votes') > 10000)
+                           .select(['tconst', 'average_rating']))
+
 # return number of null values for each column (it was used for cleaning)
 def null_values_count(df):
     cols = df.columns
