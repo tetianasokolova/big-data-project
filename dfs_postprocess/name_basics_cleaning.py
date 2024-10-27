143f3ba3abe01bd8751ca467ef83ce6140e18335
Tetiana Sokolova <tanya.sokolova1406@gmail.com>
2024-10-27 19:05:15 +0100

    Add fill_null_primary_profession function

--- dfs_postprocess/name_basics_cleaning.py
+++ dfs_postprocess/name_basics_cleaning.py
@@ -9,8 +9,32 @@ def drop_death_year_column(name_basics_df):
 def drop_primary_name_null_rows(name_basics_df):
     return name_basics_df.filter(f.col('primary_name').isNotNull())
 
+def fill_null_primary_profession(name_basics_df):
+    # split the primary_profession column to compute the mode
+    name_basics_split_df = name_basics_df.withColumn('primary_profession',
+                                               f.explode(f.split(f.col('primary_profession'), ',')))
+
+    # select primary profession row
+    profession_df = name_basics_split_df.select(f.col('primary_profession'))
+
+    # compute the count for each profession
+    profession_counts = (profession_df.groupBy('primary_profession')
+                                      .agg(f.count('*').alias('count')))
+
+    # compute the mode (the most frequent profession)
+    mode_profession_df = (profession_counts.orderBy(f.col('count').desc())
+                                        .select('primary_profession')
+                                        .first())
+
+    mode_profession_value = mode_profession_df['primary_profession']
+
+    # fill missed values with mode
+    name_basics_df = name_basics_df.fillna(mode_profession_value, subset=['primary_profession'])
+    return name_basics_df
+
 def name_basics_cleaning(name_basics_df):
     name_basics_df = drop_birth_year_column(name_basics_df)
     name_basics_df = drop_death_year_column(name_basics_df)
     name_basics_df = drop_primary_name_null_rows(name_basics_df)
+    name_basics_df = fill_null_primary_profession(name_basics_df)
     return name_basics_df
