eadfdce6a400291f083ce958a6337c17f4d1e8e2
Tetiana Sokolova <tanya.sokolova1406@gmail.com>
2024-10-27 17:39:36 +0100

    Add name_basics_cleaning function

--- dfs_postprocess/name_basics_cleaning.py
+++ dfs_postprocess/name_basics_cleaning.py
@@ -5,3 +5,8 @@ def drop_birth_year_column(name_basics_df):
 
 def drop_death_year_column(name_basics_df):
     return name_basics_df.drop(f.col('death_year'))
+
+def name_basics_cleaning(name_basics_df):
+    name_basics_df = drop_birth_year_column(name_basics_df)
+    name_basics_df = drop_death_year_column(name_basics_df)
+    return name_basics_df
