84f04e949c1296723ade9d177ff39e35d1d7a9e5
Tetiana Sokolova <tanya.sokolova1406@gmail.com>
2024-10-27 17:54:18 +0100

    Add drop_primary_name_null_rows function

--- dfs_postprocess/name_basics_cleaning.py
+++ dfs_postprocess/name_basics_cleaning.py
@@ -6,7 +6,11 @@ def drop_birth_year_column(name_basics_df):
 def drop_death_year_column(name_basics_df):
     return name_basics_df.drop(f.col('death_year'))
 
+def drop_primary_name_null_rows(name_basics_df):
+    return name_basics_df.filter(f.col('primary_name').isNotNull())
+
 def name_basics_cleaning(name_basics_df):
     name_basics_df = drop_birth_year_column(name_basics_df)
     name_basics_df = drop_death_year_column(name_basics_df)
+    name_basics_df = drop_primary_name_null_rows(name_basics_df)
     return name_basics_df
