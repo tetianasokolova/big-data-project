06a147fd718391963362934c5643b8efae8f7535
Yuliia Stoliariuk <uliastolaruk@gmail.com>
2024-10-28 11:53:19 +0200

    Remove year-like values from 'season_number'

--- dfs_postprocess/title_episode_cleaning.py
+++ dfs_postprocess/title_episode_cleaning.py
@@ -1,7 +1,19 @@
+from pyspark.sql import functions as f
+
 def delete_episode_number(title_episode_df):
     title_episode_df = title_episode_df.drop("episode_number")
     return title_episode_df
 
+
+#there are rows in dataframe that looks like year (2003, 1990 etc.)
+def delete_error_values_from_season_number(title_episode_df):
+    title_episode_df = title_episode_df.withColumn(
+        "season_number",
+        f.when(f.col("season_number") > 1000, None).otherwise(f.col("season_number")))
+    return title_episode_df
+
+
 def cleaning_title_episode(title_episode_df):
     title_episode_df = delete_episode_number(title_episode_df)
+    title_episode_df = delete_error_values_from_season_number(title_episode_df)
     return title_episode_df
