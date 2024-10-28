9f3b8869d0cfd187560c4c74e9be2bf14aebb14b
Yuliia Stoliariuk <uliastolaruk@gmail.com>
2024-10-28 11:49:02 +0200

    Delete column episode_number

--- dfs_postprocess/title_episode_cleaning.py
+++ dfs_postprocess/title_episode_cleaning.py
@@ -1,2 +1,7 @@
+def delete_episode_number(title_episode_df):
+    title_episode_df = title_episode_df.drop("episode_number")
+    return title_episode_df
+
 def cleaning_title_episode(title_episode_df):
-    pass
+    title_episode_df = delete_episode_number(title_episode_df)
+    return title_episode_df
