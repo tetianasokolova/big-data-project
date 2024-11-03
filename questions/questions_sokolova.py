624bdc90af1363871153a79e5bd81044fde95a4e
Tetiana Sokolova <tanya.sokolova1406@gmail.com>
2024-11-03 18:40:01 +0100

    Add count_movies_per_year function

--- questions/questions_sokolova.py
+++ questions/questions_sokolova.py
@@ -16,6 +16,16 @@ def titles_with_german_or_swiss_region(akas_df):
     return (akas_df.filter(f.col('region').isin(['DE', 'CH']))
                    .select(['title_id', 'title', 'region']))
 
+# aggregation, grouping and sorting questions
+# question 3: скільки фільмів (title_type = 'movie') було випущено в кожен рік випуску?
+def count_movies_per_year(title_basics_df):
+    count_movies_per_year_df = title_basics_df.filter((f.col('title_type') == 'movie')
+                                                       & f.col('start_year').isNotNull())
+    count_movies_per_year_df = (count_movies_per_year_df.groupBy(['start_year'])
+                                                        .agg(f.count('tconst').alias('movies_count'))
+                                                        .orderBy('start_year', ascending=True))
+    return count_movies_per_year_df
+
 # return number of null values for each column (it was used for cleaning)
 def null_values_count(df):
     cols = df.columns
