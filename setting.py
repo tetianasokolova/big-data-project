5585d5c4e646228ac2019df8848f48d82953374d
Tetiana Sokolova <tanya.sokolova1406@gmail.com>
2025-08-25 09:30:43 +0200

    Add setting.py file with constants with  path to data

---
+++ setting.py
@@ -0,0 +1,34 @@
+TITLE_PRINCIPALS_DF_PATH = 'data/title.principals.tsv.gz'
+TITLE_PRINCIPALS_RESULTS_PATH = 'data/results/title_principals'
+
+NAME_BASICS_DF_PATH = 'data/name.basics.tsv.gz'
+NAME_BASICS_DF_RESULTS_PATH = 'data/results/name_basics'
+
+AKAS_DF_PATH = 'data/title.akas.tsv.gz'
+AKAS_DF_RESULTS_PATH = 'data/results/akas'
+
+TITLE_CREW_DF_PATH = 'data/title.crew.tsv.gz'
+TITLE_CREW_DF_RESULTS_PATH = 'data/results/title_crew'
+
+TITLE_RATINGS_DF_PATH = 'data/title.ratings.tsv.gz'
+TITLE_RATINGS_DF_RESULTS_PATH = 'data/results/title_ratings'
+
+TITLE_BASICS_DF_PATH = 'data/title.basics.tsv.gz'
+TITLE_BASICS_DF_CSV_PATH = 'data/results/title_basics'
+
+TITLE_EPISODE_DF_PATH = 'data/title.episode.tsv.gz'
+TITLE_EPISODE_DF_CSV_PATH = 'data/results/title_episode'
+
+TITLE_RATING_WITH_VOTES_ABOVE_10000_PATH = 'data/results/title_rating_with_votes_above_10000'
+RAQUEL_WELCH_PRIMARY_PROFESSION_PATH = 'data/results/raquel_welch_primary_profession'
+TITLES_WITH_GERMAN_OR_SWISS_REGION_PATH = 'data/results/titles_with_german_or_swiss_region'
+
+COUNT_MOVIES_PER_YEAR_PATH = 'data/results/count_movies_per_year'
+THREE_POPULAR_PROFESSIONS_PATH = 'data/results/three_popular_professions'
+TOP_DIRECTOR_BY_FILM_COUNT_PATH = 'data/results/top_director_by_film_count'
+
+RUNTIME_DIFF_WITHIN_TITLE_TYPE_PATH = 'data/results/runtime_diff_within_title_type'
+RANK_BY_RUNTIME_WITHIN_START_YEAR_PATH = 'data/results/rank_by_runtime_within_start_year'
+
+TOP_FIVE_MOVIES_START_YEARS_PATH = 'data/results/top_five_movies_start_years'
+TV_SERIES_PER_DIRECTOR_COUNT_PATH = 'data/results/tv_series_per_director_count'
